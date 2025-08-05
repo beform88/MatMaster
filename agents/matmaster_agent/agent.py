8ffc971c21fce5d1de7023ce2326821d29a727e8
Rasic2 <1051987201@qq.com>
2025-08-05 16:25:44 +0800

    feat: use LlmAgent as Base

--- agents/matmaster_agent/agent.py
+++ agents/matmaster_agent/agent.py
@@ -1,17 +1,18 @@
-from google.adk.agents import BaseAgent
+from google.adk.agents import LlmAgent
 from opik.integrations.adk import track_adk_agent_recursive
 
 from agents.matmaster_agent.llm_config import MatMasterLlmConfig
 
 
-class MatMasterAgent(BaseAgent):
+class MatMasterAgent(LlmAgent):
     def __init__(self, llm_config):
         super().__init__(
-            model=llm_config.gpt_4o
+            model=llm_config.gpt_4o,
+            name="matmaster_agent"
         )
 
 
-def init_matmaster_agent() -> BaseAgent:
+def init_matmaster_agent() -> LlmAgent:
     matmodeler_agent = MatMasterAgent(MatMasterLlmConfig)
     track_adk_agent_recursive(matmodeler_agent, MatMasterLlmConfig.opik_tracer)
 
