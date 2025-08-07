3c368ddf653156867b4dcb509e8c8aeec4138c15
SchrodingersCattt <gmy721212@163.com>
2025-08-07 18:16:37 +0800

    chore: change url to test

--- agents/matmaster_agent/DPACalculator_agent/constant.py
+++ agents/matmaster_agent/DPACalculator_agent/constant.py
@@ -3,7 +3,7 @@ import copy
 from agents.matmaster_agent.constant import BohriumExecutor, BohriumStorge
 
 DPACalulator_AGENT_NAME = "dpa_calculator_agent"
-DPA_CALCULATOR_URL = "https://dpa-uuid1750659890.app-space.dplink.cc/sse?token=fa66cc76b6724d5590a89546772963fd"
+DPA_CALCULATOR_URL = "http://pfmx1355864.bohrium.tech:50001/sse"
 DPACalulator_BOHRIUM_EXECUTOR = copy.deepcopy(BohriumExecutor)
 DPACalulator_BOHRIUM_EXECUTOR["machine"]["remote_profile"]["image_address"] = \
     "registry.dp.tech/dptech/dpa-calculator:85b4fe74"
