2288479cbc4cb6d5ef34d3b6286fe8489fe95975
root <liuyuxiang92@gmail.com>
2025-08-07 13:52:45 +0800

    Add: Thermoelectric_agent

--- agents/matmaster_agent/agent.py
+++ agents/matmaster_agent/agent.py
@@ -6,6 +6,8 @@ from agents.matmaster_agent.callback import matmaster_before_agent
 from agents.matmaster_agent.constant import MATMASTER_AGENT_NAME
 from agents.matmaster_agent.llm_config import MatMasterLlmConfig
 from agents.matmaster_agent.piloteye_electro_agent.agent import init_piloteye_electro_agent
+from agents.matmaster_agent.thermoelectric_agent.agent import init_thermoelectric_agent
+
 from agents.matmaster_agent.prompt import GlobalInstruction, AgentInstruction, AgentDescription
 
 
@@ -13,11 +15,13 @@ class MatMasterAgent(HandleFileUploadLlmAgent):
 
     def __init__(self, llm_config):
         unielf_agent = init_piloteye_electro_agent(llm_config)
+        thermoelectric_agent = init_thermoelectric_agent(llm_config)
+        print(thermoelectric_agent.name)
 
         super().__init__(
             name=MATMASTER_AGENT_NAME,
             model=llm_config.gpt_4o,
-            sub_agents=[unielf_agent],
+            sub_agents=[unielf_agent, thermoelectric_agent],
             global_instruction=GlobalInstruction,
             instruction=AgentInstruction,
             description=AgentDescription,
