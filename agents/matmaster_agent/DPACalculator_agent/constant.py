77c973838612eac6d83815ee2c6f7daf8ae79ab6
Rasic2 <1051987201@qq.com>
2025-08-07 23:32:15 +0800

    fix: ImportError

--- agents/matmaster_agent/DPACalculator_agent/constant.py
+++ agents/matmaster_agent/DPACalculator_agent/constant.py
@@ -1,9 +1,9 @@
 import copy
 
-from agents.matmaster_agent.constant import BohriumExecutor, BohriumStorge
+from agents.matmaster_agent.constant import BohriumExecutor, BohriumStorge, CURRENT_ENV
 
 DPACalulator_AGENT_NAME = "dpa_calculator_agent"
-if CurrentEnv in ["test", "uat"]:
+if CURRENT_ENV in ["test", "uat"]:
     DPAMCPServerUrl = "http://pfmx1355864.bohrium.tech:50001/sse"
 else:
     DPAMCPServerUrl = "https://dpa-uuid1750659890.app-space.dplink.cc/sse?token=fa66cc76b6724d5590a89546772963fd"
