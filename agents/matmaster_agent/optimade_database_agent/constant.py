d00b372e85d9641615065296a174e1b884db07ec
NingWang <84500213+NingWang-art@users.noreply.github.com>
2025-08-08 19:00:56 +0800

    add mp oqmd jarvis nmd mpds cmr alexandria omdb odbx database

--- agents/matmaster_agent/optimade_database_agent/constant.py
+++ agents/matmaster_agent/optimade_database_agent/constant.py
@@ -1,2 +1,2 @@
 OPTIMADE_DATABASE_AGENT_NAME = "Optimade_Agent"
-OPTIMADE_URL = "http://jjxr1366132.bohrium.tech:50001/sse"
+OPTIMADE_URL = "https://material-data-retriever-uuid1754467958.app-space.dplink.cc/sse?token=da492c56028540f5ba995d33f76baf8e"
