8aa2e2539fcac8743c4df4e1f91bc3c90952050d
M.-Y. GUO <guomy26@mail2.sysu.edu.cn>
2025-08-08 17:35:31 +0800

    update links

--- agents/matmaster_agent/INVAR_agent/constant.py
+++ agents/matmaster_agent/INVAR_agent/constant.py
@@ -6,9 +6,9 @@ INVAR_AGENT_NAME = "invar_agent"
 if CURRENT_ENV in ["test", "uat"]:
     INVARMCPServerUrl = "http://pfmx1355864.bohrium.tech:50004/sse"
 else:
-    INVARMCPServerUrl = "https://dart-uuid1754393230.app-space.dplink.cc/sse?token=ef35b35bd266483ebc6f0db75751ab54"
+    INVARMCPServerUrl = "https://dart-uuid1754393230.app-space.dplink.cc/sse?token=0480762b8539410c919723276c2c05fc"
 
 INVAR_BOHRIUM_EXECUTOR = copy.deepcopy(BohriumExecutor)
 INVAR_BOHRIUM_EXECUTOR["machine"]["remote_profile"]["image_address"] = \
-    "registry.dp.tech/dptech/dpa-calculator:81a7d87e"
+    "registry.dp.tech/dptech/dpa-calculator:ddbc2642"
 INVAR_BOHRIUM_STORAGE = copy.deepcopy(BohriumStorge)
