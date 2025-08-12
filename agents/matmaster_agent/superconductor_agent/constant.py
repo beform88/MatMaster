de787f2e0cd15c314430472e3f442fccba025608
Yuxiang Liu <liuyuxiang92@gmail.com>
2025-08-12 19:17:44 +0800

    Update mcp hub url in thermoelectric and superconductor sub-agent

--- agents/matmaster_agent/superconductor_agent/constant.py
+++ agents/matmaster_agent/superconductor_agent/constant.py
@@ -1,6 +1,10 @@
+from agents.matmaster_agent.constant import CURRENT_ENV
 
 
 SuperconductorAgentName = "superconductor_agent"
-SuperconductorServerUrl="http://root@mllo1368252.bohrium.tech:50002/sse"
+if CURRENT_ENV in ["test", "uat"]:
+   SuperconductorServerUrl="http://root@mllo1368252.bohrium.tech:50002/sse"
+else:
+   SuperconductorServerUrl="https://6de4bfe9504589a457d6e92fae4f9613.app-space.dplink.cc/sse?token=92019901c5e04d12b35ebbfb6276e34e"
 
 	
