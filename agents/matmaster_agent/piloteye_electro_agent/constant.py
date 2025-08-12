edb6cae0bfd8a051a6e5232ed070a5afb1298226
chensa <chensa@dp.tech>
2025-08-12 18:03:24 +0800

    fix: machine type to c2m8

--- agents/matmaster_agent/piloteye_electro_agent/constant.py
+++ agents/matmaster_agent/piloteye_electro_agent/constant.py
@@ -7,4 +7,6 @@ PILOTEYE_SERVER_URL = "http://nlig1368433.bohrium.tech:50001/sse"
 PILOTEYE_BOHRIUM_EXECUTOR = copy.deepcopy(BohriumExecutor)
 PILOTEYE_BOHRIUM_EXECUTOR["machine"]["remote_profile"]["image_address"] = \
     "registry.dp.tech/dptech/dp/native/prod-13375/piloteye:mcpv03"
+PILOTEYE_BOHRIUM_EXECUTOR["machine"]["remote_profile"]["machine_type"] = \
+    "c2_m8_cpu"
 PILOTEYE_BOHRIUM_STORAGE = copy.deepcopy(BohriumStorge)
