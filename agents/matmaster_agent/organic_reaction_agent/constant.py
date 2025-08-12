b1fbd7c25a36d39fcdc724fd7a7936250be3da1e
xhxu <202018131158@cqu.edu.cn>
2025-08-12 17:29:46 +0800

    fix(matmaster): update organic reaction server URL based on environment

--- agents/matmaster_agent/organic_reaction_agent/constant.py
+++ agents/matmaster_agent/organic_reaction_agent/constant.py
@@ -1,11 +1,15 @@
 import copy
 
-from agents.matmaster_agent.constant import BohriumExecutor, BohriumStorge
+from agents.matmaster_agent.constant import BohriumExecutor, BohriumStorge, CURRENT_ENV
 
 ORGANIC_REACTION_AGENT_NAME = "organic_reaction_agent"
-ORGANIC_REACTION_SERVER_URL = "http://iynz1366856.bohrium.tech:50001/sse"
+if CURRENT_ENV in ["test", "uat"]:
+    ORGANIC_REACTION_SERVER_URL = "http://iynz1366856.bohrium.tech:50001/sse"
+else:
+    ORGANIC_REACTION_SERVER_URL = "https://1f187c8bc462403c4646ab271007edf4.app-space.dplink.cc/sse?token=aca7d1ad24ef436faa4470eaea006c12"
+
 
 ORGANIC_REACTION_BOHRIUM_EXECUTOR = copy.deepcopy(BohriumExecutor)
 ORGANIC_REACTION_BOHRIUM_EXECUTOR["machine"]["remote_profile"]["image_address"] = \
-    "registry.dp.tech/dptech/dp/native/prod-13364/autots:0.0.2"
+    "registry.dp.tech/dptech/dp/native/prod-13364/autots:0.1.0"
 ORGANIC_REACTION_BOHRIUM_STORAGE = copy.deepcopy(BohriumStorge)
