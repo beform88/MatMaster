71f98a658d8acbb9c42687f1741c87a16e6de3a6
YHM <yanhaoming@outlook.com>
2025-08-13 20:50:53 +0800

    change perovskite agent to two functions: 1. plot tools 2. a rag tool that can search the database and return fuzzy match result with detailed parameters.

--- agents/matmaster_agent/perovskite_agent/constant.py
+++ agents/matmaster_agent/perovskite_agent/constant.py
@@ -8,6 +8,6 @@ PerovskiteAgentName = "perovskite_plot_agent"
 # MCP Server URL for Perovskite Plot server
 
 if CURRENT_ENV in ["test", "uat"]:
-    PEROVSKITE_PLOT_URL = "http://cbqz1338812.bohrium.tech:50001/sse"
+    PEROVSKITE_PLOT_URL = "http://cbqz1338812.bohrium.tech:50003/sse"
 else:
-    PEROVSKITE_PLOT_URL = "http://cbqz1338812.bohrium.tech:50001/sse"
+    PEROVSKITE_PLOT_URL = "http://cbqz1338812.bohrium.tech:50004/sse"
