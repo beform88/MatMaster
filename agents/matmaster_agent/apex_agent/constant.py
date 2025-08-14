bff1f99464f6899dc05953f96504577c87935b97
letian <chenletian@dp.tech>
2025-08-14 16:33:46 +0800

    fix:update test&prod image, and uv.lock

--- agents/matmaster_agent/apex_agent/constant.py
+++ agents/matmaster_agent/apex_agent/constant.py
@@ -13,8 +13,8 @@ ApexAgentName = "apex_agent"
 if CURRENT_ENV in ["test", "uat"]:
     ApexServerUrl = "http://hoxz1368496.bohrium.tech:50001/sse"
 else:
-    #ApexServerUrl = "http://hoxz1368496.bohrium.tech:50001/sse"
-    ApexServerUrl = "https://apex-prime-uuid1754990126.app-space.dplink.cc/sse?token=85c6ee4ceb12412e9c920a9d35388830"
+    ApexServerUrl = "http://hoxz1368496.bohrium.tech:50001/sse"
+    #ApexServerUrl = "https://apex-prime-uuid1754990126.app-space.dplink.cc/sse?token=85c6ee4ceb12412e9c920a9d35388830"
 
 # APEX专用的Bohrium执行器配置
 ApexBohriumExecutor = copy.deepcopy(BohriumExecutor)
@@ -22,11 +22,11 @@ ApexBohriumExecutor = copy.deepcopy(BohriumExecutor)
 if CURRENT_ENV in ["test", "uat"]:
     # test
     ApexBohriumExecutor["machine"]["remote_profile"]["image_address"] = \
-        "registry.dp.tech/dptech/dp/native/prod-16664/apex-agent-test:0.1.0"
+        "registry.dp.tech/dptech/dp/native/prod-16664/apex-agent-test:0.1.1"
 else:
     # prod
     ApexBohriumExecutor["machine"]["remote_profile"]["image_address"] = \
-        "registry.dp.tech/dptech/dp/native/prod-16664/apex-agent-prod:0.0.3"
+        "registry.dp.tech/dptech/dp/native/prod-16664/apex-agent-prod:0.0.4"
 
 # APEX专用的Bohrium存储配置
 ApexBohriumStorage = copy.deepcopy(BohriumStorge)
