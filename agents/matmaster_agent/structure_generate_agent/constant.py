64149459479a97d663223b162bfeb2d6ac52f326
Guolin Wan <wan.guolin@outlook.com>
2025-08-15 13:41:30 +0800

    fix two test server for StructureGenerateAgent now

--- agents/matmaster_agent/structure_generate_agent/constant.py
+++ agents/matmaster_agent/structure_generate_agent/constant.py
@@ -1,6 +1,8 @@
-
+from agents.matmaster_agent.constant import CURRENT_ENV
 
 StructureGenerateAgentName = "structure_generate_agent"
-StructureGenerateServerUrl = "http://root@wkar1368227.bohrium.tech:50001/sse"
 
-	
+if CURRENT_ENV in ["test", "uat"]:
+    StructureGenerateServerUrl = "http://root@wkar1368227.bohrium.tech:50001/sse"
+else:
+    StructureGenerateServerUrl = "http://root@ouhw1371796.bohrium.tech:50001/sse"
