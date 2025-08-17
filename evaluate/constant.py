cd6c191788792f885a1773526dfd29f46e0288de
Rasic2 <1051987201@qq.com>
2025-08-17 17:48:51 +0800

    feat: add multi-turn-answer-quality evaluate

--- evaluate/constant.py
+++ evaluate/constant.py
@@ -5,3 +5,5 @@ MATMASTER_SUBAGENT = "MatMaster SubAgent"
 TRANSFER_TO_AGENT_QUALITY = "transfer_to_agent_quality"
 
 MATMASTER_PLAN_QUALITY = "MatMaster Plan-Quality"
+
+MATMASTER_MULTI_TURN_ANSWER_QUALITY = "MatMaster Multi-Turn-Answer-QA"
--- evaluate/main.py
+++ evaluate/main.py
@@ -3,19 +3,19 @@ import time
 from opik import evaluate, Opik
 from opik.evaluation.metrics import AnswerRelevance
 
-from evaluate.base import evaluation_task
+from evaluate.base import evaluation_task, multi_turn_evaluation_task
 from evaluate.constant import TRANSFER_TO_AGENT_QUALITY, MATMASTER_SUBAGENT, MULTI_OPTION_QUALITY, \
-    MATMASTER_MULTI_OPTION, MATMASTER_PLAN_QUALITY
+    MATMASTER_MULTI_OPTION, MATMASTER_PLAN_QUALITY, MATMASTER_MULTI_TURN_ANSWER_QUALITY
 from evaluate.multi_options_quality import MultiOptionQuality
 from evaluate.transfer_to_agent_quality import TransferToAgentQuality
 
 # transfer_to_agent_quality
-evaluate(
-    experiment_name=TRANSFER_TO_AGENT_QUALITY + time.strftime("_%Y%m%d_%H%M%S"),
-    dataset=Opik().get_dataset(name=MATMASTER_SUBAGENT),
-    task=evaluation_task,
-    scoring_metrics=[TransferToAgentQuality(model="azure/gpt-4o")],
-)
+# evaluate(
+#     experiment_name=TRANSFER_TO_AGENT_QUALITY + time.strftime("_%Y%m%d_%H%M%S"),
+#     dataset=Opik().get_dataset(name=MATMASTER_SUBAGENT),
+#     task=evaluation_task,
+#     scoring_metrics=[TransferToAgentQuality(model="azure/gpt-4o")],
+# )
 
 # multi_options_quality
 # evaluate(
@@ -32,3 +32,12 @@ evaluate(
 #     task=evaluation_task,
 #     scoring_metrics=[AnswerRelevance(name=MATMASTER_PLAN_QUALITY, model="azure/gpt-4o", require_context=False)],
 # )
+
+# multi_turn_answer_quality
+evaluate(
+    experiment_name=MATMASTER_MULTI_TURN_ANSWER_QUALITY + time.strftime("_%Y%m%d_%H%M%S"),
+    dataset=Opik().get_dataset(name=MATMASTER_MULTI_TURN_ANSWER_QUALITY),
+    task=multi_turn_evaluation_task,
+    scoring_metrics=[
+        AnswerRelevance(name=MATMASTER_MULTI_TURN_ANSWER_QUALITY, model="azure/gpt-4o", require_context=False)],
+)
