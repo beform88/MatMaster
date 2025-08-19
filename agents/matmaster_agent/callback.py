89816f7a32df50b638b672b751ce4baf3a534d0e
Rasic2 <1051987201@qq.com>
2025-08-19 18:02:00 +0800

    feat: use frontend-language

--- agents/matmaster_agent/callback.py
+++ agents/matmaster_agent/callback.py
@@ -10,7 +10,7 @@ from agents.matmaster_agent.constant import FRONTEND_STATE_KEY
 async def matmaster_before_agent(callback_context: CallbackContext) -> Optional[types.Content]:
     callback_context.state[FRONTEND_STATE_KEY] = callback_context.state.get(FRONTEND_STATE_KEY, {})
     callback_context.state[FRONTEND_STATE_KEY]['biz'] = callback_context.state[FRONTEND_STATE_KEY].get('biz', {})
-    callback_context.state['target_language'] = "zh"
+    callback_context.state['target_language'] = callback_context.state[FRONTEND_STATE_KEY].get('target_language', "zh")
     callback_context.state['current_time'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     callback_context.state["long_running_ids"] = callback_context.state.get("long_running_ids", [])
     callback_context.state["long_running_jobs"] = callback_context.state.get("long_running_jobs", {})
