aa15f3fc1adbc4e2f2ddd086155873200e996aea
Rasic2 <1051987201@qq.com>
2025-09-09 11:40:31 +0800

    feat: add MatMaster-Answer-Quality

---
+++ evaluate/experiments/answer_qa_exp.py
@@ -0,0 +1,25 @@
+import time
+
+from opik import Opik
+from opik import evaluate
+from opik.evaluation.metrics import AnswerRelevance
+
+from evaluate.base import evaluation_task
+from evaluate.utils import load_dataset_json
+
+MATMASTER_ANSWER_QUALITY = "MatMaster-Answer-Quality"
+
+# Get or create a dataset
+client = Opik()
+
+dataset = client.get_or_create_dataset(name=MATMASTER_ANSWER_QUALITY)
+dataset.clear()
+dataset_json = load_dataset_json("answer_qa_exp.json")
+dataset.insert_from_json(dataset_json)
+
+evaluate(
+    experiment_name=MATMASTER_ANSWER_QUALITY + time.strftime("_%Y%m%d_%H%M%S"),
+    dataset=dataset,
+    task=evaluation_task,
+    scoring_metrics=[AnswerRelevance(name=MATMASTER_ANSWER_QUALITY, model="azure/gpt-4o", require_context=False)],
+)
