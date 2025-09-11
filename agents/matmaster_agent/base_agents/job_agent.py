059b5415ee6bb8fe3b7dcdfed6add6df8f8f05dc
Rasic2 <1051987201@qq.com>
2025-09-11 11:57:52 +0800

    feat: update submit_valiator_msg

--- agents/matmaster_agent/base_agents/job_agent.py
+++ agents/matmaster_agent/base_agents/job_agent.py
@@ -399,8 +399,8 @@ class SubmitValidatorAgent(LlmAgent):
             ctx.session.state["long_running_jobs_count_ori"] = ctx.session.state["long_running_jobs_count"]
             await update_session_state(ctx, self.name)
         else:
-            submit_validator_msg = ("The system is currently validating the parameters. "
-                                    "Please verify that they are correct. Submission will proceed once confirmed.")
+            submit_validator_msg = ("System is experiencing task submission hallucination; "
+                                    "I recommend retrying with the original parameters.")
 
         for function_event in context_function_event(ctx, self.name, "system_submit_validator",
                                                      {"msg": submit_validator_msg},
