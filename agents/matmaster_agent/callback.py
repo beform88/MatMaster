bd5bbf55935f5afdd9e22568218de31bae6bb658
Rasic2 <1051987201@qq.com>
2025-09-15 13:25:38 +0800

    feat: implement dynamic target language detection using contextual user content in callback logic

--- agents/matmaster_agent/callback.py
+++ agents/matmaster_agent/callback.py
@@ -11,8 +11,8 @@ from google.genai import types
 from google.genai.types import FunctionCall, Part
 
 from agents.matmaster_agent.constant import FRONTEND_STATE_KEY
-from agents.matmaster_agent.model import TransferCheck
-from agents.matmaster_agent.prompt import get_transfer_check_prompt
+from agents.matmaster_agent.model import TransferCheck, UserContent
+from agents.matmaster_agent.prompt import get_transfer_check_prompt, get_user_content_lang
 from agents.matmaster_agent.utils.llm_response_utils import has_function_call
 
 logger = logging.getLogger(__name__)
@@ -25,7 +25,6 @@ async def matmaster_prepare_state(callback_context: CallbackContext) -> Optional
 
     callback_context.state[FRONTEND_STATE_KEY] = callback_context.state.get(FRONTEND_STATE_KEY, {})
     callback_context.state[FRONTEND_STATE_KEY]['biz'] = callback_context.state[FRONTEND_STATE_KEY].get('biz', {})
-    callback_context.state['target_language'] = callback_context.state[FRONTEND_STATE_KEY].get('target_language', 'zh')
     callback_context.state['long_running_ids'] = callback_context.state.get('long_running_ids', [])
     callback_context.state['long_running_jobs'] = callback_context.state.get('long_running_jobs', {})
     callback_context.state['long_running_jobs_count'] = callback_context.state.get('long_running_jobs_count', 0)
@@ -38,6 +37,15 @@ async def matmaster_prepare_state(callback_context: CallbackContext) -> Optional
     callback_context.state['sync_tools'] = callback_context.state.get('sync_tools', None)
     callback_context.state['invocation_id_with_tool_call'] = callback_context.state.get('invocation_id_with_tool_call',
                                                                                         None)
+
+    user_content = callback_context.user_content.parts[0].text
+    prompt = get_user_content_lang().format(user_content=user_content)
+    response = litellm.completion(model='azure/gpt-4o', messages=[{'role': 'user', 'content': prompt}],
+                                  response_format=UserContent)
+    result: dict = json.loads(response.choices[0].message.content)
+    logger.info(f"[matmaster_prepare_state] user_content = {result}")
+    language = str(result.get('language', "zh"))
+    callback_context.state['target_language'] = language
 
 
 # after_model_callback
