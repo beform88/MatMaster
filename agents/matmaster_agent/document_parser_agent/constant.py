74e6ac77f87f059d30a976ba9405afd0ecf2e8f3
M.-Y. GUO <guomy26@mail2.sysu.edu.cn>
2025-09-18 16:56:13 +0800

    feat: update mcp hub link

--- agents/matmaster_agent/document_parser_agent/constant.py
+++ agents/matmaster_agent/document_parser_agent/constant.py
@@ -5,4 +5,4 @@ DocumentParserAgentName = "document_parser_agent"
 if CURRENT_ENV in ["test", "uat"]:
     DocumentParserServerUrl = "http://pfmx1355864.bohrium.tech:50005/sse"
 else:
-    DocumentParserServerUrl = "https://document-parser-mcp-uuid1753018122.app-space.dplink.cc/sse?token=e93e98d3c408478d9ff113b809de690d"
+    DocumentParserServerUrl = "https://document-parser-uuid1758175282.app-space.dplink.cc/sse?token=4c9924cbb1b94bd5b74758a105e11024"
