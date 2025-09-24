a2acfd18032e6a76dabcd91b78e92703cb50da09
NingWang <84500213+NingWang-art@users.noreply.github.com>
2025-09-24 17:18:50 +0800

    feat: Mr-Dice_v0.82 add Mofdb agent

--- agents/matmaster_agent/MrDice_agent/openlam_agent/constant.py
+++ agents/matmaster_agent/MrDice_agent/openlam_agent/constant.py
@@ -3,7 +3,7 @@ from agents.matmaster_agent.constant import CURRENT_ENV
 OPENLAM_DATABASE_AGENT_NAME = 'openlam_agent'
 
 if CURRENT_ENV in ['test', 'uat']:
-    # OPTIMADE_URL="http://bekc1366122.bohrium.tech:50001/sse"
-    OPENLAM_URL = 'http://jjxr1366132.bohrium.tech:50002/sse'
+    OPENLAM_URL="http://bekc1366122.bohrium.tech:50002/sse"
+    # OPENLAM_URL = 'http://jjxr1366132.bohrium.tech:50002/sse'
 else:
     OPENLAM_URL = 'https://mr-dice-openlam-uuid1758096918.app-space.dplink.cc/sse?token=c2c0edb9c99f454896c4965ef1a5cf28'
