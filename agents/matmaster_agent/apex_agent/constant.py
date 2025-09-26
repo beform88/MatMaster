71e39aba73a3d412cff3d4985e26d83dbb1a681a
letian <chenletian@dp.tech>
2025-09-26 22:55:04 +0800

    feat: change cluster to acs for APEX testing

--- agents/matmaster_agent/apex_agent/constant.py
+++ agents/matmaster_agent/apex_agent/constant.py
@@ -10,15 +10,16 @@ if CURRENT_ENV in ['test', 'uat']:
     ApexServerUrl = 'http://ysjl1373062.bohrium.tech:50001/sse'
 else:
     # ApexServerUrl = "http://ysjl1373062.bohrium.tech:50001/sse"
-    ApexServerUrl = "https://apex-prime-uuid1754990126.app-space.dplink.cc/sse?token=7ab16179304d4a9b84cb270ff97b2979"
+    ApexServerUrl = 'https://apex-prime-uuid1754990126.app-space.dplink.cc/sse?token=7ab16179304d4a9b84cb270ff97b2979'
 # APEX专用的Bohrium执行器配置
 ApexBohriumExecutor = copy.deepcopy(BohriumExecutor)
-ApexBohriumExecutor["machine"]["remote_profile"]["machine_type"] = "c2_m8_cpu"
+ApexBohriumExecutor['machine']['remote_profile']['machine_type'] = 'c2_m8_cpu'
 
 if CURRENT_ENV in ['test', 'uat']:
     # test
-    ApexBohriumExecutor["machine"]["remote_profile"]["image_address"] = \
-        "registry.dp.tech/dptech/dp/native/prod-16664/apex-agent-test-acs:0.0.4"
+    ApexBohriumExecutor['machine']['remote_profile'][
+        'image_address'
+    ] = 'registry.dp.tech/dptech/dp/native/prod-16664/apex-agent-test-acs:0.0.4'
 else:
     # prod
     ApexBohriumExecutor['machine']['remote_profile'][
