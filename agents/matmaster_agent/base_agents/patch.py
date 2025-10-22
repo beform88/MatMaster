0438c8b263f327b377c7a48456ebef63760dabb2
Rasic2 <1051987201@qq.com>
2025-10-22 23:56:15 +0800

    feat: add detailed logging and tracing to llm call handling process

--- agents/matmaster_agent/base_agents/patch.py
+++ agents/matmaster_agent/base_agents/patch.py
@@ -4,13 +4,19 @@ import time
 from contextlib import contextmanager
 from typing import AsyncGenerator
 
-from google.adk.agents import InvocationContext, LlmAgent
+from google.adk.agents import InvocationContext, LiveRequestQueue, LlmAgent
 from google.adk.agents.base_agent import BaseAgentState
+from google.adk.agents.run_config import StreamingMode
 from google.adk.events import Event
-from google.adk.flows.llm_flows.base_llm_flow import BaseLlmFlow
+from google.adk.flows.llm_flows.base_llm_flow import (
+    _ADK_AGENT_NAME_LABEL_KEY,
+    BaseLlmFlow,
+)
 from google.adk.flows.llm_flows.single_flow import SingleFlow
-from google.adk.models import LlmRequest
+from google.adk.models import LlmRequest, LlmResponse
+from google.adk.telemetry import trace_call_llm, tracer
 from google.adk.utils.context_utils import Aclosing
+from google.genai import types
 
 from agents.matmaster_agent.constant import MATMASTER_AGENT_NAME
 
@@ -22,6 +28,153 @@ def patch_run_async_impl():
     original_LlmAgent_run_async_impl = LlmAgent._run_async_impl
     original_SingleFlow_run_async = SingleFlow.run_async
     original_BaseLlmFlow_run_one_step_async = BaseLlmFlow._run_one_step_async
+    original_BaseLlmFlow_call_llm_async = BaseLlmFlow._call_llm_async
+
+    async def patched_BaseLlmFlow_call_llm_async(
+        self,
+        invocation_context: InvocationContext,
+        llm_request: LlmRequest,
+        model_response_event: Event,
+    ) -> AsyncGenerator[LlmResponse, None]:
+        logger.info(
+            f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_call_llm_async Start {time.time()}'
+        )
+        # Runs before_model_callback if it exists.
+        if response := await self._handle_before_model_callback(
+            invocation_context, llm_request, model_response_event
+        ):
+            yield response
+            return
+
+        llm_request.config = llm_request.config or types.GenerateContentConfig()
+        llm_request.config.labels = llm_request.config.labels or {}
+
+        # Add agent name as a label to the llm_request. This will help with slicing
+        # the billing reports on a per-agent basis.
+        if _ADK_AGENT_NAME_LABEL_KEY not in llm_request.config.labels:
+            llm_request.config.labels[_ADK_AGENT_NAME_LABEL_KEY] = (
+                invocation_context.agent.name
+            )
+
+        # Calls the LLM.
+        logger.info(
+            f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_call_llm_async before __get_llm {time.time()}'
+        )
+        if hasattr(self, '_BaseLlmFlow__get_llm'):
+            llm = self._BaseLlmFlow__get_llm(invocation_context)
+        logger.info(
+            f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_call_llm_async after __get_llm {time.time()}'
+        )
+
+        async def _call_llm_with_tracing() -> AsyncGenerator[LlmResponse, None]:
+            with tracer.start_as_current_span('call_llm'):
+                if invocation_context.run_config.support_cfc:
+                    invocation_context.live_request_queue = LiveRequestQueue()
+                    responses_generator = self.run_live(invocation_context)
+                    async with Aclosing(
+                        self._run_and_handle_error(
+                            responses_generator,
+                            invocation_context,
+                            llm_request,
+                            model_response_event,
+                        )
+                    ) as agen:
+                        async for llm_response in agen:
+                            # Runs after_model_callback if it exists.
+                            if altered_llm_response := await self._handle_after_model_callback(
+                                invocation_context, llm_response, model_response_event
+                            ):
+                                llm_response = altered_llm_response
+                            # only yield partial response in SSE streaming mode
+                            if (
+                                invocation_context.run_config.streaming_mode
+                                == StreamingMode.SSE
+                                or not llm_response.partial
+                            ):
+                                yield llm_response
+                            if llm_response.turn_complete:
+                                invocation_context.live_request_queue.close()
+                else:
+                    # Check if we can make this llm call or not. If the current call
+                    # pushes the counter beyond the max set value, then the execution is
+                    # stopped right here, and exception is thrown.
+                    invocation_context.increment_llm_call_count()
+                    responses_generator = llm.generate_content_async(
+                        llm_request,
+                        stream=invocation_context.run_config.streaming_mode
+                        == StreamingMode.SSE,
+                    )
+                    logger.info(
+                        f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_call_llm_async before _run_and_handle_error {time.time()}'
+                    )
+                    async with Aclosing(
+                        self._run_and_handle_error(
+                            responses_generator,
+                            invocation_context,
+                            llm_request,
+                            model_response_event,
+                        )
+                    ) as agen:
+                        logger.info(
+                            f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_call_llm_async before _run_and_handle_error agen {time.time()}'
+                        )
+                        async for llm_response in agen:
+                            logger.info(
+                                f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_call_llm_async before trace_call_llm {time.time()}'
+                            )
+                            trace_call_llm(
+                                invocation_context,
+                                model_response_event.id,
+                                llm_request,
+                                llm_response,
+                            )
+                            logger.info(
+                                f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_call_llm_async after trace_call_llm {time.time()}'
+                            )
+                            logger.info(
+                                f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_call_llm_async before _handle_after_model_callback {time.time()}'
+                            )
+                            # Runs after_model_callback if it exists.
+                            if altered_llm_response := await self._handle_after_model_callback(
+                                invocation_context, llm_response, model_response_event
+                            ):
+                                llm_response = altered_llm_response
+                            logger.info(
+                                f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_call_llm_async after _handle_after_model_callback {time.time()}'
+                            )
+                            yield llm_response
+                        logger.info(
+                            f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_call_llm_async after _run_and_handle_error agen {time.time()}'
+                        )
+                    logger.info(
+                        f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_call_llm_async after _run_and_handle_error {time.time()}'
+                    )
+
+        logger.info(
+            f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_call_llm_async before _call_llm_with_tracing {time.time()}'
+        )
+        async with Aclosing(_call_llm_with_tracing()) as agen:
+            logger.info(
+                f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_call_llm_async before agen-for {time.time()}'
+            )
+            async for event in agen:
+                logger.info(
+                    f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_call_llm_async before agen-event {time.time()}'
+                )
+                yield event
+                logger.info(
+                    f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_call_llm_async after agen-event {time.time()}'
+                )
+            logger.info(
+                f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_call_llm_async after agen-for {time.time()}'
+            )
+        logger.info(
+            f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_call_llm_async after _call_llm_with_tracing {time.time()}'
+        )
+
+        logger.info(
+            f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_call_llm_async End {time.time()}'
+        )
 
     async def patched_BaseLlmFlow_run_one_step_async(
         self,
@@ -31,17 +184,8 @@ def patch_run_async_impl():
         logger.info(
             f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_run_one_step_async Start {time.time()}'
         )
-        logger.info(
-            f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_run_one_step_async before llm_request {time.time()}'
-        )
         llm_request = LlmRequest()
-        logger.info(
-            f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_run_one_step_async after llm_request {time.time()}'
-        )
 
-        logger.info(
-            f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_run_one_step_async before _preprocess_async {time.time()}'
-        )
         # Preprocess before calling the LLM.
         async with Aclosing(
             self._preprocess_async(invocation_context, llm_request)
@@ -50,21 +194,12 @@ def patch_run_async_impl():
                 yield event
         if invocation_context.end_invocation:
             return
-        logger.info(
-            f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_run_one_step_async after _preprocess_async {time.time()}'
-        )
 
-        logger.info(
-            f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_run_one_step_async before _get_events {time.time()}'
-        )
         # Resume the LLM agent based on the last event from the current branch.
         # 1. User content: continue the normal flow
         # 2. Function call: call the tool and get the response event.
         events = invocation_context._get_events(
             current_invocation=True, current_branch=True
-        )
-        logger.info(
-            f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_run_one_step_async after _get_events {time.time()}'
         )
         if (
             invocation_context.is_resumable
@@ -82,9 +217,6 @@ def patch_run_async_impl():
                     yield event
                 return
 
-        logger.info(
-            f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_run_one_step_async before _call_llm_async {time.time()}'
-        )
         # Calls the LLM.
         model_response_event = Event(
             id=Event.new_id(),
@@ -92,50 +224,49 @@ def patch_run_async_impl():
             author=invocation_context.agent.name,
             branch=invocation_context.branch,
         )
-        async with Aclosing(
-            self._call_llm_async(invocation_context, llm_request, model_response_event)
-        ) as agen:
+
+        BaseLlmFlow._call_llm_async = patched_BaseLlmFlow_call_llm_async
+        try:
             logger.info(
-                f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_run_one_step_async before agen {time.time()}'
+                f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_run_one_step_async before _call_llm_async {time.time()}'
             )
-            async for llm_response in agen:
-                logger.info(
-                    f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_run_one_step_async before _postprocess_async {time.time()}'
+            async with Aclosing(
+                self._call_llm_async(
+                    invocation_context, llm_request, model_response_event
                 )
-                # Postprocess after calling the LLM.
-                async with Aclosing(
-                    self._postprocess_async(
-                        invocation_context,
-                        llm_request,
-                        llm_response,
-                        model_response_event,
-                    )
-                ) as agen:
+            ) as agen:
+                logger.info(
+                    f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_run_one_step_async before agen {time.time()}'
+                )
+                # +3s
+                async for llm_response in agen:  # time cost
                     logger.info(
-                        f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_run_one_step_async before agen-for {time.time()}'
+                        f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_run_one_step_async before _postprocess_async {time.time()}'
                     )
-                    async for event in agen:
-                        # Update the mutable event id to avoid conflict
-                        model_response_event.id = Event.new_id()
-                        model_response_event.timestamp = (
-                            datetime.datetime.now().timestamp()
+                    # Postprocess after calling the LLM.
+                    async with Aclosing(
+                        self._postprocess_async(
+                            invocation_context,
+                            llm_request,
+                            llm_response,
+                            model_response_event,
                         )
-                        logger.info(
-                            f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_run_one_step_async before agen-event {time.time()}'
-                        )
-                        yield event
-                        logger.info(
-                            f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_run_one_step_async after agen-event {time.time()}'
-                        )
+                    ) as agen:
+                        async for event in agen:
+                            # Update the mutable event id to avoid conflict
+                            model_response_event.id = Event.new_id()
+                            model_response_event.timestamp = (
+                                datetime.datetime.now().timestamp()
+                            )
+                            yield event
                     logger.info(
-                        f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_run_one_step_async after agen-for {time.time()}'
+                        f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_run_one_step_async after _postprocess_async {time.time()}'
                     )
                 logger.info(
-                    f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_run_one_step_async after _postprocess_async {time.time()}'
+                    f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_run_one_step_async after agen {time.time()}'
                 )
-            logger.info(
-                f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_run_one_step_async after agen {time.time()}'
-            )
+        finally:
+            BaseLlmFlow._call_llm_async = original_BaseLlmFlow_call_llm_async
         logger.info(
             f'[{MATMASTER_AGENT_NAME}] [Timing] Patched patched_BaseLlmFlow_run_one_step_async after _call_llm_async {time.time()}'
         )
