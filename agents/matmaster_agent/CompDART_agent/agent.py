8d64f39994105a7626a4f11b8d378baa1d57e67e
M.-Y. GUO <guomy26@mail2.sysu.edu.cn>
2025-10-27 19:31:20 +0800

    fix pydantic error

--- agents/matmaster_agent/CompDART_agent/agent.py
+++ agents/matmaster_agent/CompDART_agent/agent.py
@@ -29,10 +29,10 @@ mcp_tools_compdrt = CalculationMCPToolset(
 class CompDARTAgent(BaseAsyncJobAgent):
     def __init__(self, llm_config):
         super().__init__(
-            agent_name=COMPDART_AGENT_NAME,
+            name=COMPDART_AGENT_NAME,
             mcp_tools=[mcp_tools_compdrt],
             model=llm_config.gpt_5_chat,
-            agent_description=CompDARTAgentDescription,
+            description=CompDARTAgentDescription,
             agent_instruction=CompDARTAgentInstruction,
             dflow_flag=False,
             supervisor_agent=MATMASTER_AGENT_NAME,
