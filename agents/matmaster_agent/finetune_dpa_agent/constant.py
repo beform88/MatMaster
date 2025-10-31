803cce5a8a4efffa7ea8d2bd7d9e501f2fb2a953
Yuxiang Liu <liuyuxiang92@gmail.com>
2025-10-31 09:43:22 +0800

    feat: Update published url in finetune dpa agent

--- agents/matmaster_agent/finetune_dpa_agent/constant.py
+++ agents/matmaster_agent/finetune_dpa_agent/constant.py
@@ -5,4 +5,4 @@ FinetuneDPAAgentName = 'finetune_dpa_agent'
 if CURRENT_ENV in ['test', 'uat']:
     FinetuneDPAServerUrl = 'http://root@mllo1368252.bohrium.tech:50003/sse'
 else:
-    FinetuneDPAServerUrl = 'https://b3af1c7992383988a4bd73c7e9f14759.app-space.dplink.cc/sse?token=869418403c974181be9b051a2fe1dc96'
+    FinetuneDPAServerUrl = 'https://dpafinetune-uuid1761721868.app-space.dplink.cc/sse?token=336ee47a05e5407899fdeb50c9e806d0'
