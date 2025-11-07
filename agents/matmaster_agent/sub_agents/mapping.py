a073771233f0c0f08ab0083146ae84c366e81ed8
Rasic2 <1051987201@qq.com>
2025-11-07 23:01:14 +0800

    refactor: replace ALL_AGENT_TOOLS_DICT with ALL_TOOLS for streamlined access and simplify agent tool retrieval

--- agents/matmaster_agent/sub_agents/mapping.py
+++ agents/matmaster_agent/sub_agents/mapping.py
@@ -1,5 +1,4 @@
 from enum import Enum
-from typing import Dict, List
 
 from agents.matmaster_agent.sub_agents.ABACUS_agent.agent import (
     ABACUSCalculatorAgent,
@@ -184,161 +183,166 @@ AGENT_CLASS_MAPPING = {
     TrajAnalysisAgentName: TrajAnalysisAgent,
 }
 
-
-class ABACUSAgentToolEnum(str, Enum):
-    ModifyInput = 'abacus_modify_input'
-    ModifyStructure = 'abacus_modify_stru'
-    CollectData = 'abacus_collect_data'
-    CalculationSCF = 'abacus_calculation_scf'
-    BaderChargeRun = 'abacus_badercharge_run'
-    CalculateBand = 'abacus_cal_band'
-    CalculateELF = 'abacus_cal_elf'
-    CalculateChargeDensityDifference = 'abacus_cal_charge_density_difference'
-    CalculateSpinDensity = 'abacus_cal_spin_density'
-    DOSRun = 'abacus_dos_run'
-    CalculateElastic = 'abacus_cal_elastic'
-    EOS = 'abacus_eos'
-    PhononDispersion = 'abacus_phonon_dispersion'
-    DoRelax = 'abacus_do_relax'
-    VibrationAnalysis = 'abacus_vibration_analysis'
-    RunMD = 'abacus_run_md'
-    RunABACUSCalculation = 'run_abacus_calculation'
-
-
-class APEXAgentToolEnum(str, Enum):
-    CalculateVacancy = 'apex_calculate_vacancy'
-    OptimizeStructure = 'apex_optimize_structure'
-    CalculateInterstitial = 'apex_calculate_interstitial'
-    CalculateElastic = 'apex_calculate_elastic'
-    CalculateSurface = 'apex_calculate_surface'
-    CalculateEOS = 'apex_calculate_eos'
-    CalculatePhonon = 'apex_calculate_phonon'
-    CalculateGamma = 'apex_calculate_gamma'
-
-
-class ChemBrainAgentToolEnum(str, Enum):
-    GetTargetInfo = 'get_target_info'
-    UniELFInference = 'unielf_inference'
-    PlanAndVisualizeReaction = 'plan_and_visualize_reaction'
-    ConvertSMILESToPNG = 'convert_smiles_to_png'
-    ConvertPNGToSMILES = 'convert_png_to_smiles'
-    ValidateSMILES = 'validate_smiles'
-
-
-class CompDartAgentToolEnum(str, Enum):
-    RunGA = 'run_ga'
-
-
-class DocumentParserAgentToolEnum(str, Enum):
-    ExtractMaterialDataFromPDF = 'extract_material_data_from_pdf'
-
-
-class DPAAgentToolEnum(str, Enum):
-    OptimizeStructure = 'optimize_structure'
-    RunMolecularDynamics = 'run_molecular_dynamics'
-    CalculatePhonon = 'calculate_phonon'
-    CalculateElasticConstants = 'calculate_elastic_constants'
-    RunNEB = 'run_neb'
-
-
-class FinetuneDPAAgentToolEnum(str, Enum):
-    FinetuneDPAModel = 'finetune_dpa_model'
-
-
-class HEAAssistantAgentToolEnum(str, Enum):
-    ParamsCalculator = 'HEA_params_calculator'
-    Predictor = 'HEA_predictor'
-
-
-class HEACalculatorAgentToolEnum(str, Enum):
-    GenerateBinaryPhaseDiagram = 'generate_binary_phase_diagram'
-
-
-class MrDiceAgentToolEnum(str, Enum):
-    FetchStructuresWithFilter = 'fetch_structures_with_filter'
-    FetchStructuresWithSpg = 'fetch_structures_with_spg'
-    FetchStructuresWithBandgap = 'fetch_structures_with_bandgap'
-    FetchBohriumCrystals = 'fetch_bohrium_crystals'
-    FetchOpenlamStructures = 'fetch_openlam_structures'
-    FetchMofsSql = 'fetch_mofs_sql'
-
-
-class OrganicReactionAgentToolEnum(str, Enum):
-    CalculateReactionProfile = 'calculate_reaction_profile'
-
-
-class PerovskiteAgentToolEnum(str, Enum):
-    SemanticSearch = 'semantic_search'
-    PlotVsTime = 'plot_vs_time'
-
-
-class PilotEyeElectroAgentToolEnum(str, Enum):
-    RunPilotEye = 'run_piloteye'
-
-
-class SSEBrainAgentToolEnum(str, Enum):
-    DeepResearchAgent = 'deep_research_agent'
-    DatabaseAgent = 'database_agent'
-
-
-class StructureGenerateAgentToolEnum(str, Enum):
-    GenerateCalypsoStructures = 'generate_calypso_structures'
-    GenerateCrystalformerStructures = 'generate_crystalformer_structures'
-    MakeSupercellStructure = 'make_supercell_structure'
-    BuildBulkStructureByTemplate = 'build_bulk_structure_by_template'
-    BuildMoleculeStructureFromG2Database = 'build_molecule_structure_from_g2database'
-    BuildSurfaceSlab = 'build_surface_slab'
-    BuildSurfaceAdsorbate = 'build_surface_adsorbate'
-    BuildSurfaceInterface = 'build_surface_interface'
-    AddCellForMolecules = 'add_cell_for_molecules'
-    BuildBulkStructureByWyckoff = 'build_bulk_structure_by_wyckoff'
-    BuildMoleculeStructuresFromSMILES = 'build_molecule_structures_from_smiles'
-    MakeDopedStructure = 'make_doped_structure'
-    MakeAmorphousStructure = 'make_amorphous_structure'
-    GetStructureInfo = 'get_structure_info'
-
-
-class SuperconductorAgentToolEnum(str, Enum):
-    RunSuperconductorOptimization = 'run_superconductor_optimization'
-    CalculateSuperconductorEnthalpy = 'calculate_superconductor_enthalpy'
-    PredictSuperconductorTc = 'predict_superconductor_Tc'
-    ScreenSuperconductor = 'screen_superconductor'
-
-
-class ThermoelectricAgentToolEnum(str, Enum):
-    PredictThermoelectricProperties = 'predict_thermoelectric_properties'
-    RunPressureOptimization = 'run_pressure_optimization'
-    CalculateThermoelectricEnthalpy = 'calculate_thermoele_enthalp'
-    ScreenThermoelectricCandidate = 'screen_thermoelectric_candidate'
-
-
-class TrajAnalysisAgentToolEnum(str, Enum):
-    TrajAnalysisMSD = 'traj_analysis_msd'
-    TrajAnalysisRDF = 'traj_analysis_rdf'
-    TrajAnalysisSolvation = 'traj_analysis_solvation'
-    TrajAnalysisBond = 'traj_analysis_bond'
-    TrajAnalysisReact = 'traj_analysis_react'
-
-
-AGENT_TOOLS_MAPPING = {
-    ABACUS_AGENT_NAME: ABACUSAgentToolEnum,
-    ApexAgentName: APEXAgentToolEnum,
-    CHEMBRAIN_AGENT_NAME: ChemBrainAgentToolEnum,
-    COMPDART_AGENT_NAME: CompDartAgentToolEnum,
-    DocumentParserAgentName: DocumentParserAgentToolEnum,
-    DPACalulator_AGENT_NAME: DPAAgentToolEnum,
-    FinetuneDPAAgentName: FinetuneDPAAgentToolEnum,
-    HEA_assistant_AgentName: HEAAssistantAgentToolEnum,
-    HEACALCULATOR_AGENT_NAME: HEACalculatorAgentToolEnum,
-    MrDice_Agent_Name: MrDiceAgentToolEnum,
-    ORGANIC_REACTION_AGENT_NAME: OrganicReactionAgentToolEnum,
-    PerovskiteAgentName: PerovskiteAgentToolEnum,
-    PILOTEYE_ELECTRO_AGENT_NAME: PilotEyeElectroAgentToolEnum,
-    SSEBRAIN_AGENT_NAME: SSEBrainAgentToolEnum,
-    StructureGenerateAgentName: StructureGenerateAgentToolEnum,
-    SuperconductorAgentName: SuperconductorAgentToolEnum,
-    ThermoelectricAgentName: ThermoelectricAgentToolEnum,
-    TrajAnalysisAgentName: TrajAnalysisAgentToolEnum,
+ALL_TOOLS = {
+    'abacus_modify_input': {'belonging_agent': ABACUS_AGENT_NAME, 'scene': []},
+    'abacus_modify_stru': {'belonging_agent': ABACUS_AGENT_NAME, 'scene': []},
+    'abacus_collect_data': {'belonging_agent': ABACUS_AGENT_NAME, 'scene': []},
+    'abacus_calculation_scf': {'belonging_agent': ABACUS_AGENT_NAME, 'scene': []},
+    'abacus_badercharge_run': {'belonging_agent': ABACUS_AGENT_NAME, 'scene': []},
+    'abacus_cal_band': {'belonging_agent': ABACUS_AGENT_NAME, 'scene': []},
+    'abacus_cal_elf': {'belonging_agent': ABACUS_AGENT_NAME, 'scene': []},
+    'abacus_cal_charge_density_difference': {
+        'belonging_agent': ABACUS_AGENT_NAME,
+        'scene': [],
+    },
+    'abacus_cal_spin_density': {'belonging_agent': ABACUS_AGENT_NAME, 'scene': []},
+    'abacus_dos_run': {'belonging_agent': ABACUS_AGENT_NAME, 'scene': []},
+    'abacus_cal_elastic': {'belonging_agent': ABACUS_AGENT_NAME, 'scene': []},
+    'abacus_eos': {'belonging_agent': ABACUS_AGENT_NAME, 'scene': []},
+    'abacus_phonon_dispersion': {'belonging_agent': ABACUS_AGENT_NAME, 'scene': []},
+    'abacus_do_relax': {'belonging_agent': ABACUS_AGENT_NAME, 'scene': []},
+    'abacus_vibration_analysis': {'belonging_agent': ABACUS_AGENT_NAME, 'scene': []},
+    'abacus_run_md': {'belonging_agent': ABACUS_AGENT_NAME, 'scene': []},
+    'run_abacus_calculation': {'belonging_agent': ABACUS_AGENT_NAME, 'scene': []},
+    'apex_calculate_vacancy': {'belonging_agent': ApexAgentName, 'scene': []},
+    'apex_optimize_structure': {'belonging_agent': ApexAgentName, 'scene': []},
+    'apex_calculate_interstitial': {'belonging_agent': ApexAgentName, 'scene': []},
+    'apex_calculate_elastic': {'belonging_agent': ApexAgentName, 'scene': []},
+    'apex_calculate_surface': {'belonging_agent': ApexAgentName, 'scene': []},
+    'apex_calculate_eos': {'belonging_agent': ApexAgentName, 'scene': []},
+    'apex_calculate_phonon': {'belonging_agent': ApexAgentName, 'scene': []},
+    'apex_calculate_gamma': {'belonging_agent': ApexAgentName, 'scene': []},
+    'get_target_info': {'belonging_agent': CHEMBRAIN_AGENT_NAME, 'scene': []},
+    'unielf_inference': {'belonging_agent': CHEMBRAIN_AGENT_NAME, 'scene': []},
+    'plan_and_visualize_reaction': {
+        'belonging_agent': CHEMBRAIN_AGENT_NAME,
+        'scene': [],
+    },
+    'convert_smiles_to_png': {'belonging_agent': CHEMBRAIN_AGENT_NAME, 'scene': []},
+    'convert_png_to_smiles': {'belonging_agent': CHEMBRAIN_AGENT_NAME, 'scene': []},
+    'validate_smiles': {'belonging_agent': CHEMBRAIN_AGENT_NAME, 'scene': []},
+    'run_ga': {'belonging_agent': COMPDART_AGENT_NAME, 'scene': []},
+    'extract_material_data_from_pdf': {
+        'belonging_agent': DocumentParserAgentName,
+        'scene': [],
+    },
+    'optimize_structure': {'belonging_agent': DPACalulator_AGENT_NAME, 'scene': []},
+    'run_molecular_dynamics': {'belonging_agent': DPACalulator_AGENT_NAME, 'scene': []},
+    'calculate_phonon': {'belonging_agent': DPACalulator_AGENT_NAME, 'scene': []},
+    'calculate_elastic_constants': {
+        'belonging_agent': DPACalulator_AGENT_NAME,
+        'scene': [],
+    },
+    'run_neb': {'belonging_agent': DPACalulator_AGENT_NAME, 'scene': []},
+    'finetune_dpa_model': {'belonging_agent': FinetuneDPAAgentName, 'scene': []},
+    'HEA_params_calculator': {'belonging_agent': HEA_assistant_AgentName, 'scene': []},
+    'HEA_predictor': {'belonging_agent': HEA_assistant_AgentName, 'scene': []},
+    'generate_binary_phase_diagram': {
+        'belonging_agent': HEACALCULATOR_AGENT_NAME,
+        'scene': [],
+    },
+    'fetch_structures_with_filter': {'belonging_agent': MrDice_Agent_Name, 'scene': []},
+    'fetch_structures_with_spg': {'belonging_agent': MrDice_Agent_Name, 'scene': []},
+    'fetch_structures_with_bandgap': {
+        'belonging_agent': MrDice_Agent_Name,
+        'scene': [],
+    },
+    'fetch_bohrium_crystals': {'belonging_agent': MrDice_Agent_Name, 'scene': []},
+    'fetch_openlam_structures': {'belonging_agent': MrDice_Agent_Name, 'scene': []},
+    'fetch_mofs_sql': {'belonging_agent': MrDice_Agent_Name, 'scene': []},
+    'calculate_reaction_profile': {
+        'belonging_agent': ORGANIC_REACTION_AGENT_NAME,
+        'scene': [],
+    },
+    'semantic_search': {'belonging_agent': PerovskiteAgentName, 'scene': []},
+    'plot_vs_time': {'belonging_agent': PerovskiteAgentName, 'scene': []},
+    'run_piloteye': {'belonging_agent': PILOTEYE_ELECTRO_AGENT_NAME, 'scene': []},
+    'deep_research_agent': {'belonging_agent': SSEBRAIN_AGENT_NAME, 'scene': []},
+    'database_agent': {'belonging_agent': SSEBRAIN_AGENT_NAME, 'scene': []},
+    'generate_calypso_structures': {
+        'belonging_agent': StructureGenerateAgentName,
+        'scene': [],
+    },
+    'generate_crystalformer_structures': {
+        'belonging_agent': StructureGenerateAgentName,
+        'scene': [],
+    },
+    'make_supercell_structure': {
+        'belonging_agent': StructureGenerateAgentName,
+        'scene': [],
+    },
+    'build_bulk_structure_by_template': {
+        'belonging_agent': StructureGenerateAgentName,
+        'scene': [],
+    },
+    'build_molecule_structure_from_g2database': {
+        'belonging_agent': StructureGenerateAgentName,
+        'scene': [],
+    },
+    'build_surface_slab': {'belonging_agent': StructureGenerateAgentName, 'scene': []},
+    'build_surface_adsorbate': {
+        'belonging_agent': StructureGenerateAgentName,
+        'scene': [],
+    },
+    'build_surface_interface': {
+        'belonging_agent': StructureGenerateAgentName,
+        'scene': [],
+    },
+    'add_cell_for_molecules': {
+        'belonging_agent': StructureGenerateAgentName,
+        'scene': [],
+    },
+    'build_bulk_structure_by_wyckoff': {
+        'belonging_agent': StructureGenerateAgentName,
+        'scene': [],
+    },
+    'build_molecule_structures_from_smiles': {
+        'belonging_agent': StructureGenerateAgentName,
+        'scene': [],
+    },
+    'make_doped_structure': {
+        'belonging_agent': StructureGenerateAgentName,
+        'scene': [],
+    },
+    'make_amorphous_structure': {
+        'belonging_agent': StructureGenerateAgentName,
+        'scene': [],
+    },
+    'get_structure_info': {'belonging_agent': StructureGenerateAgentName, 'scene': []},
+    'run_superconductor_optimization': {
+        'belonging_agent': SuperconductorAgentName,
+        'scene': [],
+    },
+    'calculate_superconductor_enthalpy': {
+        'belonging_agent': SuperconductorAgentName,
+        'scene': [],
+    },
+    'predict_superconductor_Tc': {
+        'belonging_agent': SuperconductorAgentName,
+        'scene': [],
+    },
+    'screen_superconductor': {'belonging_agent': SuperconductorAgentName, 'scene': []},
+    'predict_thermoelectric_properties': {
+        'belonging_agent': ThermoelectricAgentName,
+        'scene': [],
+    },
+    'run_pressure_optimization': {
+        'belonging_agent': ThermoelectricAgentName,
+        'scene': [],
+    },
+    'calculate_thermoele_enthalp': {
+        'belonging_agent': ThermoelectricAgentName,
+        'scene': [],
+    },
+    'screen_thermoelectric_candidate': {
+        'belonging_agent': ThermoelectricAgentName,
+        'scene': [],
+    },
+    'traj_analysis_msd': {'belonging_agent': TrajAnalysisAgentName, 'scene': []},
+    'traj_analysis_rdf': {'belonging_agent': TrajAnalysisAgentName, 'scene': []},
+    'traj_analysis_solvation': {'belonging_agent': TrajAnalysisAgentName, 'scene': []},
+    'traj_analysis_bond': {'belonging_agent': TrajAnalysisAgentName, 'scene': []},
+    'traj_analysis_react': {'belonging_agent': TrajAnalysisAgentName, 'scene': []},
 }
 
 
@@ -363,29 +367,5 @@ class MatMasterSubAgentsEnum(str, Enum):
     TrajAnalysisAgent = TrajAnalysisAgentName
     FinetuneDPAAgent = FinetuneDPAAgentName
 
-    # 第二层：具体的工具枚举映射
-    @property
-    def tools(self):
-        return AGENT_TOOLS_MAPPING.get(self.value)
 
-    @classmethod
-    def get_all_tools_list(cls) -> List[str]:
-        """获取所有agent和工具的对应字典"""
-        result = []
-        for category in cls:
-            if category.tools:
-                result += [tool.value for tool in category.tools]
-        return result
-
-    @classmethod
-    def get_all_tools_dict(cls) -> Dict[str, Dict[str, str]]:
-        """获取所有agent和工具的对应字典"""
-        result = {}
-        for category in cls:
-            if category.tools:
-                result[category.value] = [tool.value for tool in category.tools]
-        return result
-
-
-ALL_AGENT_TOOLS_DICT = MatMasterSubAgentsEnum.get_all_tools_dict()
-ALL_AGENT_TOOLS_LIST = MatMasterSubAgentsEnum.get_all_tools_list()
+ALL_AGENT_TOOLS_LIST = list(ALL_TOOLS.keys())
