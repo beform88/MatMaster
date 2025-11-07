340f6f0a2684bc249b93db1dc3c12061990ea095
Rasic2 <1051987201@qq.com>
2025-11-07 16:27:44 +0800

    refactor: consolidate agent configurations and remove redundant base class

--- agents/matmaster_agent/sub_agents/chembrain_agent/retrosyn_agent/agent.py
+++ agents/matmaster_agent/sub_agents/chembrain_agent/retrosyn_agent/agent.py
@@ -1,10 +1,11 @@
 from dp.agent.adapter.adk import CalculationMCPToolset
 from google.adk.tools.mcp_tool.mcp_toolset import SseConnectionParams
 
+from agents.matmaster_agent.base_agents.public_agent import BaseSyncAgent
 from agents.matmaster_agent.constant import BohriumStorge
 from agents.matmaster_agent.llm_config import MatMasterLlmConfig
 
-from ..base import CalculationLlmAgent
+from ..constant import CHEMBRAIN_AGENT_NAME
 from .callback import retrosyn_after_tool_transform_tgz
 from .constant import RetrosynAgentName, RetrosynServerUrl
 from .prompt import description, instruction_en
@@ -18,13 +19,14 @@ retrosyn_toolset = CalculationMCPToolset(
 def init_retrosyn_agent(llm_config):
     selected_model = llm_config.gpt_4o
 
-    retrosyn_agent = CalculationLlmAgent(
+    retrosyn_agent = BaseSyncAgent(
         model=selected_model,
         name=RetrosynAgentName,
         description=description,
         instruction=instruction_en,
         tools=[retrosyn_toolset],
         after_tool_callback=[retrosyn_after_tool_transform_tgz],
+        supervisor_agent=CHEMBRAIN_AGENT_NAME,
     )
     return retrosyn_agent
 
