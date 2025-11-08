a59526fbabffacfc45e9cb00ecf4a50435970373
Rasic2 <1051987201@qq.com>
2025-11-08 22:49:51 +0800

    feat: add DisallowTransferAgent to prevent object transfers to parent or peers

---
+++ agents/matmaster_agent/base_agents/disallow_transfer_agent.py
@@ -0,0 +1,23 @@
+from typing import Any
+
+from pydantic import model_validator
+
+from agents.matmaster_agent.base_agents.abc_agent import BaseMixin
+from agents.matmaster_agent.base_agents.error_agent import ErrorHandleLlmAgent
+
+
+class DisallowTransferMixin(BaseMixin):
+    @model_validator(mode='before')
+    @classmethod
+    def decorate_callbacks(cls, data: Any) -> Any:
+        if not isinstance(data, dict):
+            return data
+
+        data['disallow_transfer_to_parent'] = True
+        data['disallow_transfer_to_peers'] = True
+
+        return data
+
+
+class DisallowTransferAgent(DisallowTransferMixin, ErrorHandleLlmAgent):
+    pass
