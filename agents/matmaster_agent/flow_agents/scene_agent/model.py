c1b8f2294961cc6052b0faedf94eff8e667c6d60
Rasic2 <1051987201@qq.com>
2025-11-10 12:49:01 +0800

    feat: add calculation in SceneEnum

--- agents/matmaster_agent/flow_agents/scene_agent/model.py
+++ agents/matmaster_agent/flow_agents/scene_agent/model.py
@@ -4,4 +4,5 @@ from enum import Enum
 class SceneEnum(str, Enum):
     STRUCTURE_GENERATE = 'structure_generate'
     DATABASE_SEARCH = 'database_search'
+    Calculation = 'calculation'
     JobResultRetrieval = 'job_result_retrieval'
