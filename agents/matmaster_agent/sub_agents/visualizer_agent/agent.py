3eb911ad9282dc0d17320507763f40bac67fc816
M.-Y. GUO <guomy26@mail2.sysu.edu.cn>
2025-11-12 17:34:04 +0800

    feat: add visualization agent

---
+++ agents/matmaster_agent/sub_agents/visualizer_agent/agent.py
@@ -0,0 +1,32 @@
+from dp.agent.adapter.adk import CalculationMCPToolset
+from google.adk.agents import BaseAgent
+from google.adk.tools.mcp_tool.mcp_session_manager import SseServerParams
+
+from agents.matmaster_agent.base_agents.public_agent import BaseSyncAgent
+from agents.matmaster_agent.constant import MATMASTER_AGENT_NAME, BohriumStorge
+from agents.matmaster_agent.llm_config import LLMConfig
+
+from .constant import VisualizerAgentName, VisualizerServerUrl
+from .prompt import VisualizerAgentDescription, VisualizerAgentInstruction
+
+sse_params = SseServerParams(url=VisualizerServerUrl)
+visualizer_toolset = CalculationMCPToolset(
+    connection_params=sse_params, storage=BohriumStorge
+)
+
+
+class VisualizerAgent(BaseSyncAgent):
+    def __init__(self, llm_config: LLMConfig):
+        super().__init__(
+            model=llm_config.default_litellm_model,
+            name=VisualizerAgentName,
+            description=VisualizerAgentDescription,
+            instruction=VisualizerAgentInstruction,
+            tools=[visualizer_toolset],
+            supervisor_agent=MATMASTER_AGENT_NAME,
+        )
+
+
+def init_visualizer_agent(llm_config) -> BaseAgent:
+    """Initialize Visualizer Agent"""
+    return VisualizerAgent(llm_config)
