c833e7ce84dd4848908975b6773495038cf206f2
Rasic2 <1051987201@qq.com>
2025-11-16 18:05:24 +0800

    feat: add global logger setup to improve logging consistency across agents and enforce single function call for job agents

--- agents/matmaster_agent/logger.py
+++ agents/matmaster_agent/logger.py
@@ -1,4 +1,6 @@
 import logging
+from datetime import datetime
+from pathlib import Path
 
 from google.adk.tools.tool_context import ToolContext
 from mcp import types
@@ -28,3 +30,26 @@ class PrefixFilter(logging.Filter):
     def filter(self, record):
         record.msg = f"[{self.prefix}] {record.msg}"
         return True
+
+
+def setup_global_logger():
+    root_logger = logging.getLogger()
+
+    # 创建文件处理器
+    LOG_DIR = Path('../logs')
+    LOG_DIR.mkdir(parents=True, exist_ok=True)
+
+    log_filename = datetime.now().strftime('%Y-%m-%d') + '.log'
+    file_handler = logging.FileHandler(LOG_DIR / log_filename)  # 文件名可以自定义
+    file_handler.setLevel(logging.INFO)
+
+    # 创建格式化器并设置给处理器
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+    file_handler.setFormatter(formatter)
+
+    # 将文件处理器添加到 logger
+    root_logger.addHandler(file_handler)
+
+    return root_logger
