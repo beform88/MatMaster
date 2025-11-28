726022dcc7cedffe2edf47a5e89017ff67ad251c
letian <chenletian@dp.tech>
2025-11-28 17:21:10 +0800

    fix: nmr agent return format

--- agents/matmaster_agent/sub_agents/NMR_agent/constant.py
+++ agents/matmaster_agent/sub_agents/NMR_agent/constant.py
@@ -1,4 +1,6 @@
-from agents.matmaster_agent.constant import CURRENT_ENV
+import copy
+
+from agents.matmaster_agent.constant import CURRENT_ENV, BohriumStorge
 
 NMR_AGENT_NAME = 'nmr_agent'
 if CURRENT_ENV in ['test', 'uat']:
@@ -7,3 +9,6 @@ if CURRENT_ENV in ['test', 'uat']:
 else:
     NMR_MCP_SERVER_URL = 'http://aubn1408899.bohrium.tech:50001/sse'
     # NMR_MCP_SERVER_URL = 'https://nmr-uuid1751618147.app-space.dplink.cc/sse?token=beab553bf78b4c53a7490531d14492c4'
+
+NMR_BOHRIUM_STORAGE = copy.deepcopy(BohriumStorge)
+NMR_BOHRIUM_STORAGE['type'] = 'https'
