fc5a67f18e63894a4a150d267f80e05bcdfd743c
SchrodingersCattt <gmy721212@163.com>
2025-11-30 21:23:07 +0800

    enhanced formats

--- agents/matmaster_agent/job_agents/recommend_params_agent/prompt.py
+++ agents/matmaster_agent/job_agents/recommend_params_agent/prompt.py
@@ -2,9 +2,15 @@ def gen_recommend_params_agent_instruction():
     return """
 Convert the recommended parameters into a user-friendly format for a tool invocation.
 
+- The output language must be {target_language}.
+
+## FORMAT INSTRUCTIONS:
+- A space should be added between figures and units, e.g. 10 cm, 5 kg. An italic font should be used for physical quantities. A bold font should be used for vectors;
+- Chemical formula should be appropriately formatted using superscript and subscript, not plain text;
+- Space group should be written in the format of appropriate `H-M` notation with Latin letters in intalics and correct subscript for screw axis, not plain text.
+
 Critical Instructions:
 - If the input parameter is a **complete HTTP or HTTPS URL**, use it directly as the parameter.
-- The output language must be {target_language}.
 - Present the information in a clear, readable style, such as a list or natural paragraphs.
 - Do not end with any question or prompt for user action.
 """
