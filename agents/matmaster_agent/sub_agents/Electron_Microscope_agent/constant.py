4f27d5a0b91a2412548c0e19a283052e696e4810
594233 <692810802@qq.com>
2025-12-01 20:47:29 +0800

    rename_ElectronMicroscopeAgent

--- agents/matmaster_agent/sub_agents/Electron_Microscope_agent/constant.py
+++ agents/matmaster_agent/sub_agents/Electron_Microscope_agent/constant.py
@@ -2,12 +2,12 @@ import copy
 
 from agents.matmaster_agent.constant import CURRENT_ENV, BohriumStorge
 
-EM_AGENT_NAME = 'em_agent'
+Electron_Microscope_AGENT_NAME = 'electron_microscope_agent'
 if CURRENT_ENV in ['test', 'uat']:
-    EM_MCP_SERVER_URL = 'http://root@pkfz1410356.bohrium.tech:50002/sse'
+    Electron_Microscope_MCP_SERVER_URL = 'http://root@pkfz1410356.bohrium.tech:50002/sse'
     # NMR_MCP_SERVER_URL = 'https://nmr-uuid1751618147.app-space.dplink.cc/sse?token=beab553bf78b4c53a7490531d14492c4'
 else:
-    EM_MCP_SERVER_URL = 'http://root@pkfz1410356.bohrium.tech:50002/sse'
+    Electron_Microscope_MCP_SERVER_URL = 'http://root@pkfz1410356.bohrium.tech:50002/sse'
     # NMR_MCP_SERVER_URL = 'https://nmr-uuid1751618147.app-space.dplink.cc/sse?token=beab553bf78b4c53a7490531d14492c4'
 
-EM_BOHRIUM_STORAGE = copy.deepcopy(BohriumStorge)
+Electron_Microscope_BOHRIUM_STORAGE = copy.deepcopy(BohriumStorge)
