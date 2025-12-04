6ec37a4ce6b76d75c5f05c7d86a4166b753e3a2d
SchrodingersCattt <gmy721212@163.com>
2025-12-04 16:56:20 +0800

    fix typo

--- agents/matmaster_agent/sub_agents/structure_generate_agent/constant.py
+++ agents/matmaster_agent/sub_agents/structure_generate_agent/constant.py
@@ -2,7 +2,7 @@ from agents.matmaster_agent.constant import CURRENT_ENV
 
 StructureGenerateAgentName = 'structure_generate_agent'
 
-if CURRENT_ENV in ['test', 'uat']:
+if CURRENT_ENV == 'test':
     StructureGenerateServerUrl = 'http://qpus1389933.bohrium.tech:50003/sse'
 elif CURRENT_ENV == 'uat':
     StructureGenerateServerUrl = 'https://93b71ff0836bb95466a9de7aaa34de09.appspace.uat.bohrium.com/sse?token=c46b37df6b9e46ce88986afa120b7f34'
