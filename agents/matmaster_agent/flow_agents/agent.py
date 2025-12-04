e5bda85fbedf9e3f9b1e4a408e56136ff47c15d0
styxhuang <huangmingwilliam@gmail.com>
2025-12-04 15:50:36 +0800

    refactor: convert follow_up_list to comma-separated string before passing to context function

--- agents/matmaster_agent/flow_agents/agent.py
+++ agents/matmaster_agent/flow_agents/agent.py
@@ -478,6 +478,7 @@ class MatMasterFlowAgent(LlmAgent):
                 pass
             else:
                 follow_up_list = _follow_up_questions.get('list', [])
+                follow_up_str = ",".join(follow_up_list)
                 for generate_follow_up_event in context_function_event(
                     ctx,
                     self.name,
@@ -487,7 +488,7 @@ class MatMasterFlowAgent(LlmAgent):
                     {
                         'invocation_id': ctx.invocation_id,
                         'title': follow_up_title,
-                        'follow_up_list': follow_up_list,
+                        'follow_up_list': follow_up_str,
                     },
                 ):
                     yield generate_follow_up_event
