6d988791dcc5aa88864d735cbebd6d55a237bde6
letian <chenletian@dp.tech>
2025-12-05 10:24:35 +0800

    fix: change nmr prod server to mcphub

--- agents/matmaster_agent/sub_agents/NMR_agent/constant.py
+++ agents/matmaster_agent/sub_agents/NMR_agent/constant.py
@@ -6,7 +6,7 @@ NMR_AGENT_NAME = 'nmr_agent'
 if CURRENT_ENV in ['test', 'uat']:
     NMR_MCP_SERVER_URL = 'http://aubn1408899.bohrium.tech:50001/sse'
 else:
-    NMR_MCP_SERVER_URL = 'http://aubn1408899.bohrium.tech:50001/sse'
-    # NMR_MCP_SERVER_URL = 'https://nmr-server-matmaster-uuid1764741165.appspace.bohrium.com/sse?token=1467bc01801642c09273966fcd04e3a6'
+    # NMR_MCP_SERVER_URL = 'http://aubn1408899.bohrium.tech:50001/sse'
+    NMR_MCP_SERVER_URL = 'https://nmr-server-matmaster-uuid1764741165.appspace.bohrium.com/sse?token=1467bc01801642c09273966fcd04e3a6'
 
 NMR_BOHRIUM_STORAGE = copy.deepcopy(BohriumStorge)
