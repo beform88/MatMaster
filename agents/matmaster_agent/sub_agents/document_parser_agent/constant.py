3d5a73465207d652c352363f6004718dcef5ca94
SchrodingersCattt <gmy721212@163.com>
2025-12-12 12:03:19 +0800

    fix: fallback to bohrium nodes for prod for doc and visualizer

--- agents/matmaster_agent/sub_agents/document_parser_agent/constant.py
+++ agents/matmaster_agent/sub_agents/document_parser_agent/constant.py
@@ -5,4 +5,5 @@ DocumentParserAgentName = 'document_parser_agent'
 if CURRENT_ENV in ['test', 'uat']:
     DocumentParserServerUrl = 'http://pfmx1355864.bohrium.tech:50005/sse'
 else:
-    DocumentParserServerUrl = 'https://document-parser-uuid1758175282.appspace.bohrium.com/sse?token=17a3a9849c1c4fde963a7dc1135aca18'
+    # DocumentParserServerUrl = 'https://document-parser-uuid1758175282.appspace.bohrium.com/sse?token=17a3a9849c1c4fde963a7dc1135aca18'
+    DocumentParserServerUrl = 'http://pfmx1355864.bohrium.tech:50001/sse'
