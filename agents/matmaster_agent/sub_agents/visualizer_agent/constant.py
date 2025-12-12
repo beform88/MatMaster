d5077f9adfb966fe10a9ce074836dbedc765bf61
SchrodingersCattt <gmy721212@163.com>
2025-12-12 17:39:52 +0800

    Revert "fix: fallback to bohrium nodes for prod for doc and visualizer"
    This reverts commit 38d633d67ea23d67dcdba261d25cf271247d5471.

--- agents/matmaster_agent/sub_agents/visualizer_agent/constant.py
+++ agents/matmaster_agent/sub_agents/visualizer_agent/constant.py
@@ -5,5 +5,4 @@ VisualizerAgentName = 'visualizer_agent'
 if CURRENT_ENV in ['test', 'uat']:
     VisualizerServerUrl = 'http://qpus1389933.bohrium.tech:50005/sse'
 else:
-    # VisualizerServerUrl = 'https://visualization-agent-uuid1762946293.appspace.bohrium.com/sse?token=0b925beda7e44f66b69047319e87967d'
-    VisualizerServerUrl = 'http://qpus1389933.bohrium.tech:50002/sse'
+    VisualizerServerUrl = 'https://visualization-agent-uuid1762946293.appspace.bohrium.com/sse?token=0b925beda7e44f66b69047319e87967d'
