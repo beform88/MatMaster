f7a185dfdfcadc9dff3469df1d22110b7949c911
SchrodingersCattt <gmy721212@163.com>
2025-12-12 10:34:03 +0800

    fix: update doc-parser prod url

--- agents/matmaster_agent/sub_agents/document_parser_agent/constant.py
+++ agents/matmaster_agent/sub_agents/document_parser_agent/constant.py
@@ -5,4 +5,4 @@ DocumentParserAgentName = 'document_parser_agent'
 if CURRENT_ENV in ['test', 'uat']:
     DocumentParserServerUrl = 'http://pfmx1355864.bohrium.tech:50005/sse'
 else:
-    DocumentParserServerUrl = 'https://document-parser-uuid1758175282.appspace.bohrium.com/sse?token=1b824a883cff4af1b1c8345d3bc693d5'
+    DocumentParserServerUrl = 'https://document-parser-uuid1758175282.appspace.bohrium.com/sse?token=17a3a9849c1c4fde963a7dc1135aca18'
