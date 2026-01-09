dc58ca2a93e603ecff16a7054fb87d776883c80e
Rasic2 <1051987201@qq.com>
2026-01-10 00:28:59 +0800

    feat: add JSON response format specification for step_title_agent instructions

--- agents/matmaster_agent/base_callbacks/private_callback.py
+++ agents/matmaster_agent/base_callbacks/private_callback.py
@@ -97,6 +97,9 @@ def filter_safety_content(func: BeforeModelCallback) -> BeforeModelCallback:
                     f'{callback_context.session.id} {callback_context.agent_name} Content too long, use latest {index+1} part'
                 )
                 break
+            if callback_context.agent_name == 'step_title_agent' and index == 0:
+                break
+
         logger.info(
             f'{callback_context.session.id} {callback_context.agent_name} index={index}, record_tokens = {record_tokens}'
         )
--- agents/matmaster_agent/flow_agents/step_title_agent/prompt.py
+++ agents/matmaster_agent/flow_agents/step_title_agent/prompt.py
@@ -1,1 +1,6 @@
-STEP_TITLE_INSTRUCTION = """"""
+STEP_TITLE_INSTRUCTION = """
+You must respond with a JSON object containing:
+{{
+    "title": string,  // title description in {target_language}
+}}
+"""
