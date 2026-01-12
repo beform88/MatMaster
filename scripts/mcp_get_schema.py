e90ae2cf2df1d1370c79443ae55953652720c777
SchrodingersCattt <gmy721212@163.com>
2026-01-12 19:26:06 +0800

    fix pre-commit

--- scripts/mcp_get_schema.py
+++ scripts/mcp_get_schema.py
@@ -8,7 +8,7 @@ from mcp.client.streamable_http import streamablehttp_client
 
 async def get_tool_function_declaration(url: str, tool_name: str, method: str = 'sse'):
     client_cm = sse_client(url) if method == 'sse' else streamablehttp_client(url)
-    
+
     async with client_cm as streams:
         # sse 长度为 2，http 长度为 3，直接取前两个
         read, write = streams[0], streams[1]
@@ -27,7 +27,9 @@ if __name__ == '__main__':
     # sse
     url = 'http://toyl1410396.bohrium.tech:50001/sse'
     asyncio.run(get_tool_function_declaration(url, tool_name='abacus_dos_run'))
-    
+
     # streamable http
-    url = "https://structure-generator-uuid1767674194.appspace.bohrium.com/mcp?token=a297dc122dc74368af3d2e725991d559"
-    asyncio.run(get_tool_function_declaration(url, tool_name='add_hydrogens', method='mcp'))
+    url = 'https://structure-generator-uuid1767674194.appspace.bohrium.com/mcp?token=a297dc122dc74368af3d2e725991d559'
+    asyncio.run(
+        get_tool_function_declaration(url, tool_name='add_hydrogens', method='mcp')
+    )
