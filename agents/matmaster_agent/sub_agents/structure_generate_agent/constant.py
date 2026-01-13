1b14c299dfdd38b0199d415ef0647d8495126404
SchrodingersCattt <gmy721212@163.com>
2026-01-13 16:04:29 +0800

    chore: update uat mcphub link

--- agents/matmaster_agent/sub_agents/structure_generate_agent/constant.py
+++ agents/matmaster_agent/sub_agents/structure_generate_agent/constant.py
@@ -5,7 +5,7 @@ StructureGenerateAgentName = 'structure_generate_agent'
 if CURRENT_ENV == 'test':
     StructureGenerateServerUrl = 'http://qpus1389933.bohrium.tech:50003/mcp'
 elif CURRENT_ENV == 'uat':
-    StructureGenerateServerUrl = 'https://93b71ff0836bb95466a9de7aaa34de09.appspace.uat.bohrium.com/sse?token=c46b37df6b9e46ce88986afa120b7f34'  # TODO: update uat url to mcp
+    StructureGenerateServerUrl = 'https://structure-generator-uat-2-uuid1767842266.appspace.uat.bohrium.com/mcp?token=45e714e2de6942829021f4c6e81971cf'
 else:
     # StructureGenerateServerUrl = 'http://pfmx1355864.bohrium.tech:50003/sse' # TODO: update backup prod url to mcp
     StructureGenerateServerUrl = 'https://structure-generator-uuid1767674194.appspace.bohrium.com/mcp?token=a297dc122dc74368af3d2e725991d559'
